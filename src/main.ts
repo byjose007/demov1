15214dc92fa314851466db3f38c13e7a87ec0920
byron.armijos <byron.armijos@col.vueling.com>
2024-10-28 03:27:50 +0100

    upda port 3200

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,20 @@ import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule, { cors: true });
   
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  //   allowedHeaders: "Content-Type"
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: "Content-Type"
 
-  //   // credentials: true,
+    // credentials: true,
 
-  // });
+  });
 
-  // app.useWebSocketAdapter(new IoAdapter(app));
+  app.useWebSocketAdapter(new IoAdapter(app));
   
-  await app.listen(3000);
+  await app.listen(3200);
 }
 bootstrap();
